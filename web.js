c1d3dbbf021e89b679771957886b3ebe28b9a6df
John Smith <willey@selby.com>
2013-07-10 20:01:00 -0700

    try 4

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('./index.html'));
 
 app.get('/', function(request, response) {
+  var buffer = new Buffer(fs.readFileSync('./index.html'));
   response.send(buffer.toString());
 });
 
