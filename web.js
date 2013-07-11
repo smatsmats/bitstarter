2ded7c90fbeb205c58d68580b049d12c859ed92a
John Smith <willey@selby.com>
2013-07-10 19:35:09 -0700

    trying to use a buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var greeting = fs.readFileSync('./index.html');
+var buffer = new Buffer(greeting)
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
